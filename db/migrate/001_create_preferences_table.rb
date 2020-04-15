12d06d798b4e30b317789fef2950b0b4f700d3a9
Jordan <jordanleelong5@gmail.com>
2020-04-14 21:28:41 -0700

    Done.

---
+++ db/migrate/001_create_preferences_table.rb
@@ -0,0 +1,9 @@
+class CreatePreferencesTable < ActiveRecord::Migration
+    def change
+        create_table :preferences do |t|
+          t.boolean :allow_create_artists
+          t.boolean :allow_create_songs
+          t.timestamps
+        end
+      end
+end
